bd491c455089ce7ae2c66d7173090d495ed0ec22
nguyennguyenloc <48286184+nguyennguyenloc@users.noreply.github.com>
2019-10-10 09:11:10 +0700

    ViewResult

---
+++ Registration/Registration/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Registration.Models;
+
+namespace Registration.Controllers
+{
+    public class HomeController : Controller
+    {
+       /* public IActionResult Index()
+        {
+            return View();
+        }*/
+        public ViewResult index()
+        {
+            return View("myView");
+        }
+        public String Index2()
+        {
+            return "Hello Lộc, you can do it";
+        }
+
+        
+    }
+}
